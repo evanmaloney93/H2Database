d889d46b42ad7f8a808df73a0bd1d6d634c7b5a8
maloneyevan2 <maloneyevan2@gmail.com>
2021-04-04 20:22:33 +0100

    Created a user entity and repo, no longer uses data.sql

--- src/main/java/com/example/demo/TestH2ProjectApplication.java
+++ src/main/java/com/example/demo/TestH2ProjectApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TestH2ProjectApplication {
@@ -9,5 +13,14 @@ public class TestH2ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestH2ProjectApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(UserRepository userRepository){
+		return args -> {
+			userRepository.save(new User("Tony", "Stark", "T.Stark@avengers.com") );
+		};
+	}
+
+
 
 }
